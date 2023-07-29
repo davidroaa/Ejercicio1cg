9a9fb5fdb71455f31397a987729eceaec5f68dac
davidroaa <140860789+davidroaa@users.noreply.github.com>
2023-07-28 19:18:54 -0500

    3
    Actualizacion de datos

--- src/vista/UICliente.java
+++ src/vista/UICliente.java
@@ -6,7 +6,9 @@
 package vista;
 
 import java.util.LinkedList;
+import javax.swing.JOptionPane;
 import modelo.Cliente;
+import modelo.Utilidades;
 
 /**
  *
@@ -45,6 +47,7 @@ public class UICliente extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -86,6 +89,13 @@ public class UICliente extends javax.swing.JFrame {
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
+        jButton4.setText("jButton4");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -97,6 +107,8 @@ public class UICliente extends javax.swing.JFrame {
                         .addComponent(jButton1)
                         .addGap(38, 38, 38)
                         .addComponent(jButton2)
+                        .addGap(133, 133, 133)
+                        .addComponent(jButton4)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(123, 123, 123)
@@ -146,7 +158,8 @@ public class UICliente extends javax.swing.JFrame {
                         .addGap(46, 46, 46)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jButton2)
+                    .addComponent(jButton4))
                 .addContainerGap(34, Short.MAX_VALUE))
         );
 
@@ -171,7 +184,23 @@ public class UICliente extends javax.swing.JFrame {
     System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    /**
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+
+
+
+
+
+
+
+boolean t = Utilidades.guardarArchivo(listaClientes);
+if (t){
+    JOptionPane.showMessageDialog(null, "se creo el archivo");
+} else{
+ JOptionPane.showMessageDialog(null, " no se creo el archivo");}
+
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    /**}
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -210,6 +239,7 @@ public class UICliente extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
